c4e7c1d568d665cc1a240c01ba4702d2ca84f5c9
Paul Mangold <paul.mangold@ens-lyon.fr>
2018-01-16 20:13:39 +0100

    Change CV folds amount to 7

--- problem.py
+++ problem.py
@@ -19,7 +19,7 @@ score_types = [
 
 
 def get_cv(X, y):
-    cv = ShuffleSplit(n_splits=2, test_size=0.2, random_state=57)
+    cv = ShuffleSplit(n_splits=7, test_size=0.2, random_state=57)
     return cv.split(X)
 
 
